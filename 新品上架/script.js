37b28d2cd8446e02b00afa7c68eb5005671fd5ae
Jiang <76017078+long-tai-0925@users.noreply.github.com>
2024-12-19 13:40:08 +0800

    Delete 新品上架 directory

--- 新品上架/script.js
+++
@@ -1,6 +0,0 @@
-
-window.addEventListener('resize', function() {
-    const contentHeight = document.getElementById('.main-content').offsetHeight;
-    const footer = document.querySelector('footer');
-    footer.style.top = contentHeight + 'px';
-  });
